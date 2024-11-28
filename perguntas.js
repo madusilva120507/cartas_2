d20cdd19ad83743039db8e64d66f49697a5ad00c
madusilva120507 <00001076531532sp@al.educacao.sp.gov.br>
2024-11-28 20:30:26 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Maquiagem',
+    ' Como escolher a base certa para o meu tom de pele?',
+    'Teste a base no maxilar, em luz natural. Ela deve desaparecer na sua pele, sem deixar marcas. Verifique também o subtom: quente, frio ou neutro.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Skin Care',
+    'Como introduzir ácidos na rotina de skin care?',
+    'Comece com concentrações baixas e em dias alternados. Use à noite e nunca se esqueça de aplicar protetor solar durante o dia.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Cabelos Cacheados',
+    'Qual é a importância da finalização para cabelos cacheados?',
+    'A finalização define os cachos, controla o frizz e mantém a hidratação. Use cremes para pentear, leave-ins ou gel para melhor definição.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Cuidados com a pele',
+    ' Qual é a importância de usar protetor solar diariamente?',
+    'O protetor solar protege a pele contra os danos causados pelos raios UV, como envelhecimento precoce, manchas, queimaduras e o risco de câncer de pele. Mesmo em dias nublados ou dentro de casa, os raios UV podem causar danos.'
 )
